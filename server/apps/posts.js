e4a49be2df035c0c862aa7c920edcfaf933a7a4b
SamitaTT <m.samita.tt@gmail.com>
2024-01-24 17:18:41 +0700

    feat: ex

--- server/apps/posts.js
+++ server/apps/posts.js
@@ -1,4 +1,5 @@
-import { Router } from "express";
+import { Router, query } from "express";
+import { pool } from "../utils/db.js";
 
 const postRouter = Router();
 
@@ -7,17 +8,32 @@ postRouter.get("/", async (req, res) => {
   const keywords = req.query.keywords;
   const page = req.query.page;
 
-  return res.json({
-    data: [],
-  });
+  try {
+    const results = await pool.query("select * from posts");
+    return res.json({
+      data: results.rows,
+    });
+  } catch (error) {
+    return res.json({
+      message: `Error`,
+    });
+  }
 });
 
 postRouter.get("/:id", async (req, res) => {
   const postId = req.params.id;
-
-  return res.json({
-    data: {},
-  });
+  try {
+    const result = await pool.query("select * from posts where post_id$1", [
+      postId,
+    ]);
+    return res.json({
+      data: result.rows[0],
+    });
+  } catch (error) {
+    return res.json({
+      message: `Error`,
+    });
+  }
 });
 
 postRouter.post("/", async (req, res) => {
@@ -28,10 +44,29 @@ postRouter.post("/", async (req, res) => {
     updated_at: new Date(),
     published_at: hasPublished ? new Date() : null,
   };
+  try {
+    const result = await pool.query(
+      "insert into posts (title, content, status, category, created_at, updated_at, published_at) values ($1, $2, $3, $4, $5, $6, $7)",
+      [
+        newPost.title,
+        newPost.content,
+        newPost.status,
+        newPost.category,
+        newPost.created_at,
+        newPost.updated_at,
+        newPost.published_at,
+      ]
+    );
 
-  return res.json({
-    message: "Post has been created.",
-  });
+    return res.json({
+      message: "Post has been created.",
+      postId: result.rows[0].id,
+    });
+  } catch (error) {
+    return res.json({
+      message: `Error`,
+    });
+  }
 });
 
 postRouter.put("/:id", async (req, res) => {
@@ -43,18 +78,42 @@ postRouter.put("/:id", async (req, res) => {
     published_at: hasPublished ? new Date() : null,
   };
   const postId = req.params.id;
-
-  return res.json({
-    message: `Post ${postId} has been updated.`,
-  });
+  try {
+    const results = await pool.query(
+      "update posts set title=$1, content=$2, status=$3, category=$4, updated_at=$5, published_at=$6  where post_id = $7 returning *",
+      [
+        updatedPost.title,
+        updatedPost.content,
+        updatedPost.status,
+        updatedPost.category,
+        updatedPost.updated_at,
+        updatedPost.published_at,
+        postId,
+      ]
+    );
+    return res.json({
+      message: `Post ${postId} has been updated.`,
+    });
+  } catch (error) {
+    return res.json({
+      message: `Error`,
+    });
+  }
 });
 
 postRouter.delete("/:id", async (req, res) => {
   const postId = req.params.id;
 
-  return res.json({
-    message: `Post ${postId} has been deleted.`,
-  });
+  try {
+    await pool.query("delete from posts where post_id=$1", [postId]);
+    return res.json({
+      message: `Post ${postId} has been deleted.`,
+    });
+  } catch (error) {
+    res.json({
+      message: `Error`,
+    });
+  }
 });
 
 export default postRouter;
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,8 @@
-// Creating PostgreSQL Client here
+import * as pg from "pg";
+const { Pool } = pg.default;
+
+const pool = new Pool({
+  connectionString: "postgresql://postgres:Server27@localhost:5432/posts",
+});
+
+export { pool };
